94f801e9ca6225102e3bca7aad9de728de75f5f3
Alex Bernt <alexbernt100@gmail.com>
2023-12-01 19:12:59 -0600

    Add partner page

---
+++ src/HomePage/Partner.jsx
@@ -0,0 +1,23 @@
+const Partner = () => {
+  return (
+    <div>
+      <div className="pb-12 max-w-5xl mx-auto">
+        <p className=" text-slate-500 text-xl pb-5">
+          What Helps Make a Great Retirement Plan?
+        </p>
+        <h1 className="font-semibold text-4xl pb-12">
+          Partner with an advisor who understands your needs and is easy to work
+          with
+        </h1>
+        <p className="text-lg">
+          We know you’re busy wearing many different hats during an average
+          workday. That’s why we made partnering with OnTrack401(k) as simple
+          and easy as possible. Our primary focus is to thoroughly understand
+          your plan’s unique needs.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Partner;
