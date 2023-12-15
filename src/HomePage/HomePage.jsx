7689c7e30cd3c2d634679c45d367eb7f00536d6c
Alex Bernt <alexbernt100@gmail.com>
2023-12-14 23:29:17 -0600

    add roadmap to home page

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Video from "./Video";
 import GreatPlan from "./GreatPlan";
 import Partner from "./Partner";
+import Roadmap from "./Roadmap";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Video />
       <GreatPlan />
       <Partner />
+      <Roadmap />
     </>
   );
 };
